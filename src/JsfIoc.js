a0ecd717fca4aa14b76147cd72ffc4c6f5394538
fschwiet <fschwiet@gmail.com>
2010-09-10 22:08:31 -0700

    added support for setting configuration parameters before service is loaded

--- src/JsfIoc.js
+++ src/JsfIoc.js
@@ -63,7 +63,13 @@ JsfIoc.prototype = {
             }
         }
 
-        if (binding.parameters instanceof Array) {
+        if (binding.boundParameters) {
+            for (var i = 0; i < binding.parameters.length; i++) {
+                var parameter = binding.parameters[i];
+                result[parameter] = binding.boundParameters[parameter];
+            }
+        } 
+        else if (binding.parameters instanceof Array) {
             for (var i = 0; i < binding.parameters.length; i++) {
                 var parameter = binding.parameters[i];
                 result[parameter] = arguments[1 + i];
@@ -75,6 +81,20 @@ JsfIoc.prototype = {
         }
 
         return result;
+    },
+    Configure: function(name) {
+
+        var binding = this._bindings[name];
+        var boundParameters = {};
+
+        if (binding.parameters instanceof Array) {
+            for (var i = 0; i < binding.parameters.length; i++) {
+                var parameter = binding.parameters[i];
+                boundParameters[parameter] = arguments[1 + i];
+            }
+        }
+
+        binding.boundParameters = boundParameters;
     }
 };
 
