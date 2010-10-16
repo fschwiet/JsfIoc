d453d972bacd2e830f248d9eea28b06b3f46deca
fschwiet <fschwiet@gmail.com>
2010-10-15 23:41:24 -0700

    this might actually work

---
+++ spec/Some.tests.js
@@ -0,0 +1,40 @@
+﻿
+
+describe("Some", function () {
+
+    it("Unique.Integer", function () {
+
+        var val1 = Unique.Integer();
+        var val2 = Unique.Integer();
+
+        expect(val1).toNotEqual(val2);
+        expect(val1).toEqual(jasmine.any(Number));
+        expect(val2).toEqual(jasmine.any(Number));
+    });
+
+    it("Unique.String", function () {
+
+        var val1 = Unique.String();
+        var val2 = Unique.String("Sample");
+
+        expect(val1).toNotEqual(val2);
+        expect(val1).toEqual(jasmine.any(String));
+        expect(val2).toEqual(jasmine.any(String));
+        expect(val2.indexOf("Sample")).not.toBeLessThan(0);
+    });
+
+    describe("Dom elements", function () {
+
+        it("creates div by default", function () {
+
+            expect(Some.DomElement().is("div")).toBeTruthy();
+        });
+
+        it("can create other tags", function () {
+
+            expect(Some.DomElement("a").is("a")).toBeTruthy();
+        });
+    });
+});
+
+
---
+++ src/Some.js
@@ -0,0 +1,53 @@
+﻿
+
+var counter = 1;
+
+Some = {
+    Integer: function () { return counter++; },
+    Number: function () { return some.Integer() + Math.random(); },
+    String: function (substring) {
+        var substring = substring || "someString"; 
+        return substring + some.Integer();
+    },
+    Object: function () { return {}; },
+    NonemptyArray: function () { return [some.String()]; },
+    Boolean: function () { return (some.Integer() % 1) == 0 ? true : false; },
+    DomElement: function (tag) {
+        tag = tag || "div";
+        return $("<" + tag + ">" + some.String() + "</" + tag + ">");
+    },
+    RawDomElement: function () {
+        return $("<div>" + some.String() + "</div>")[0];
+    },
+    Function: function () { return function () { }; },
+    ArrayOf: function (size) {
+
+        var arrayBuilder = {};
+
+        for (member in some) {
+
+            if (!this.hasOwnProperty(member))
+                continue;
+
+            var that = this;
+
+            arrayBuilder[member] = (function (originalMember) {
+                return function () {
+
+                    var arrayResult = [];
+
+                    for (var i = 0; i < size; i++) {
+                        arrayResult.push(originalMember.apply(that, arguments));
+                    }
+
+                    return arrayResult;
+                }
+            })(that[member]);
+        }
+
+        return arrayBuilder;
+    }
+};
+
+Unique = Some;
+some = Some;
