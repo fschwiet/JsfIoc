77343c5a05d6a46d28b3befd828dc307d812aa98
fschwiet <fschwiet@gmail.com>
2010-10-21 00:46:04 -0700

    redoing directory structure / build.ps1

--- build.ps1
+++ build.ps1
@@ -1,6 +1,27 @@
-rm .\build\* -recurse
 
-gci .\src\ *.js -rec | % { , ("// " + $_.name), @(get-content $_.fullname) } | set-content .\build\JsfIoc.js
-gci .\spec\ *.js -rec | % { , ("// " + $_.name), @(get-content $_.fullname) } | set-content .\build\JsfIoc.tests.js
-cp SpecRunner.html .\build\
-cp lib .\build\ -rec
+$src = @();
+$testutil = @();
+$tests = @();
+
+switch -w (gci .\src *.js -rec) {
+        "*.tests.js" { $tests = $tests + $_ }
+        default { $src = $src + $_ }
+        }
+
+switch -w (gci .\testutil *.js -rec) {
+        "*.tests.js" { $tests = $tests + $_ }
+        default { $testutil = $testutil + $_ }
+        }
+
+,"Detected source files:" + $src + "" | write-host
+
+,"Detected test utility code:" + $testutil + "" | write-host
+
+,"Detected test files:" + $tests + "" | write-host
+
+
+rm .\build -recurse
+$null = mkdir .\build
+
+$src | % { , ("// " + $_.name), @(get-content $_.fullname) } | set-content .\build\JsfIoc.js
+$testutil | % { , ("// " + $_.name), @(get-content $_.fullname) } | set-content .\build\JsfIoc.testutil.js
---
+++ lib/graphviz/run_simple.ps1
@@ -0,0 +1,1 @@
+﻿& 'C:\Program Files (x86)\Graphviz2.26.3\bin\dot.exe' -Tsvg .\simple.dot -o test.svg
