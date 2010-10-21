322144a1650a00449b8e7cd51139098d5aeb18f5
fschwiet <fschwiet@gmail.com>
2010-10-21 01:24:00 -0700

    starting graphviz formatting

---
+++ testutil/GraphVizFormatting.js
@@ -0,0 +1,19 @@
+﻿
+
+function GraphVizFormatting(ioc) {
+    this._ioc = ioc;
+}
+
+GraphVizFormatting.prototype = {
+    constructor: GraphVizFormatting,
+
+    GetBinding: function (name) {
+
+        var binding = this._ioc._bindings[name];
+
+        if (binding == null)
+            return name + ' [ shape="record"; label="' + name + ' | (instance)" ]';
+
+        return name + ' [ shape="record"; label="' + name + '" ]';
+    }
+}
---
+++ testutil/GraphVizFormatting.tests.js
@@ -0,0 +1,38 @@
+﻿/// <reference path="../lib/jasmine-0.11.1/jasmine.js" />
+/// <reference path="../Intellisense.js" />
+
+describe("GraphVizFormatting", function () {
+
+    function Foo() {
+    }
+
+    it("can format a registered instance", function () {
+
+        var ioc = new JsfIoc();
+
+        ioc.RegisterInstance("_foo", {});
+
+        var sut = new GraphVizFormatting(ioc);
+
+        var result = sut.GetBinding("_foo");
+
+        expect(result).toEqual('_foo [ shape="record"; label="_foo | (instance)" ]');
+    });
+
+    it("can format a registered service", function () {
+
+        var ioc = new JsfIoc();
+
+        ioc.Register({
+            service: Foo,
+            name: "_foo"
+        });
+
+        var sut = new GraphVizFormatting(ioc);
+
+        var result = sut.GetBinding("_foo");
+
+        expect(result).toEqual('_foo [ shape="record"; label="Foo" ]');
+    });
+
+});
