f182c5a61ec6a18afe7694c2923128c911861c68
fschwiet <fschwiet@gmail.com>
2010-12-25 14:35:03 -0800

    minor test tweak

--- src/ExtendAsFluent.tests.js
+++ src/ExtendAsFluent.tests.js
@@ -1,10 +1,8 @@
 ﻿
-
-function Foo() {
-    ExtendAsFluent.PrototypeOf(Foo);
-}
-
-(function () {
+describe("ExtendAsFluent", function() {
+    function Foo() {
+        ExtendAsFluent.PrototypeOf(Foo);
+    }
 
     //ExtendAsFluent(Foo, {
     Foo.prototype = {
@@ -20,44 +18,40 @@ function Foo() {
 
     var foo = new Foo();
 
-    describe("ExtendAsFluent", function () {
+    it("functions return values are stilled returned", function () {
 
-        it("functions return values are stilled returned", function () {
+        var expected = "123";
 
-            var expected = "123";
+        var foo = new Foo();
 
-            var foo = new Foo();
+        foo.setValue(expected);
 
-            foo.setValue(expected);
+        expect(foo.getValue()).toEqual(expected);
 
-            expect(foo.getValue()).toEqual(expected);
-
-        });
-
-        it("functions with no return value become fluent", function () {
-
-            var foo = new Foo();
-
-            expect(foo.setValue("123")).toEqual(foo);
-        });
-
-        it("non-functions are not modified", function() {
-
-            var foo = new Foo();
-
-            expect(Foo.prototype.Global).toEqual(3);
-        });
-
-        it("prototype is only extended once", function() {
-
-            var fooA = new Foo();
-
-            var firstSetValue = fooA.setValue;
-
-            var fooB = new Foo();
-
-            expect(firstSetValue).toBe(fooB.setValue);
-        });
     });
 
-})();
+    it("functions with no return value become fluent", function () {
+
+        var foo = new Foo();
+
+        expect(foo.setValue("123")).toEqual(foo);
+    });
+
+    it("non-functions are not modified", function() {
+
+        var foo = new Foo();
+
+        expect(Foo.prototype.Global).toEqual(3);
+    });
+
+    it("prototype is only extended once", function() {
+
+        var fooA = new Foo();
+
+        var firstSetValue = fooA.setValue;
+
+        var fooB = new Foo();
+
+        expect(firstSetValue).toBe(fooB.setValue);
+    });
+});
